59479ad0816958430de572055c30b50039214e38
tecnicocefet <tecnicocefet@gmail.com>
2024-04-21 21:14:21 -0300

    fix: conection refused with mongo reordering

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
+
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'users',
+    'host': 'mongodb',
+    'port': 27017,
+    'username': 'admin',
+    'password': 'admin'
+}
+
+
 api = Api(app)
+db = MongoEngine(app)
+
+
+class UserModel(db.Document):
+    cpf = db.StringField(required=True, unique=True)
+    first_name = db.StringField(required=True)
+    last_name = db.StringField(required=True)
+    email = db.StringField(required=True)
+    birth_date = db.DateTimeField(required=True)
 
 
 class Users(Resource):
